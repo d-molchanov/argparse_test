84541c77c6098775e7150507c0935f2bded0b347
Dmitry Molchanov <d.a.molchanoff@gmail.com>
2023-03-16 15:54:29 +0300

    Reading data from files has been added.

--- argparse_test.py
+++ argparse_test.py
@@ -1,6 +1,26 @@
 from os import walk
-from os.path import splitext, abspath
+from os.path import splitext, abspath, join
 from argparse import ArgumentParser
+from urllib.parse import urlparse
+
+def parse_files(files):
+    all_lines = []
+    for filename in files:
+        try:
+            with open(filename, 'r') as f:
+                data = [el.rstrip() for el in f.readlines() if el != '\n']
+                all_lines += data
+        except IOError:
+            print(f'Check {filename} - something wrong!')    
+    return all_lines
+
+def filter_files_by_extention(files, ext):
+    result = []
+    for f in  files:
+        f_name, f_ext = splitext(f)
+        if f_ext == ext or (f_name.endswith(f'.{ext}') and f_ext == ''):
+            result.append(f)
+    return result
 
 
 
@@ -9,24 +29,17 @@ if __name__ == '__main__':
     parser.add_argument('--pd')
     parser.add_argument('--ext')
     args = parser.parse_args()
-    files = None
+    files = []
     if args.pd:
         files = next(walk(args.pd))[2]
     if args.ext:
         if files:
-            ext_files = [el for el in files if splitext(el)[1] == f'.{args.ext}' or (splitext(el)[0].endswith('.txt') and splitext(el)[1] == '')]
+            ext_files = filter_files_by_extention(files, args.ext)
             print(f'*.{args.ext} files:')
             for i, el in enumerate(ext_files, 1):
                 print(i, el, sep='\t')
-            if ext_files:
-                all_lines = []
-                for f in ext_files:
-                    print(f'{args.pd}/{f}')
-                    # try:
-                    #     with open(f'./{args.pd}/{f}' 'r') as _f:
-                    #         data = [el for el in _f.readlines() if el]
-                    #         all_lines += data
-                    # except IOError:
-                    #     print(f'Check {f} - something wrong!')
-                for line in all_lines:
-                    print(line)
+            abs_files = [join(abspath(args.pd), f) for f in ext_files]
+            parsed_data = parse_files(abs_files)
+            for data in parsed_data:
+                print(data)
+
